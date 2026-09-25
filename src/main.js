a897141eedd6067c552a8bbd572a43b58f640be7
Giovanny Guerra <giovanny.guerra@mail.udp.cl>
2026-09-25 03:36:11 +0000

    Pagina crear cuenta (guardado)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 const boton = document.querySelector('#boton_login')
 const formulario = document.querySelector('#formulario')
+const boton_entrar = document.querySelector('#boton_entrar')
+const boton_cuenta = document.querySelector('#boton_cuenta')
+const boton_registar = document.querySelector('#registrar')
+const contraseña_1 = document.querySelector('contraseña_1')
+const contraseña_2 = document.querySelector('contraseña_2')
 
 boton.addEventListener("click", () =>{
     if(formulario.style.display == "none"){
@@ -10,8 +15,12 @@ boton.addEventListener("click", () =>{
     }
 })
 
-formulario.addEventListener("submit", (e)=>{
+boton_entrar.addEventListener("click", (e)=>{
   e.preventDefault()
   window.location.href = './principal.html'
 })
 
+boton_cuenta.addEventListener("click", (e)=>{
+    e.preventDefault()
+    window.location.href = './cuenta.html'
+})
